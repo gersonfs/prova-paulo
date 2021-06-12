f0ace3a5087492cc1807af489f8cb3803714765f
pauloracunha <paulo.racunha@gmail.com>
2021-06-12 19:26:56 -0300

    Fix: corrigida leitura de dados de clientes

--- src/LeitorArquivo.php
+++ src/LeitorArquivo.php
@@ -6,22 +6,46 @@ namespace App;
 
 class LeitorArquivo
 {
+    /**
+     * @var array
+     */
+    protected $data;
+
+    /**
+     * @var false|string[]
+     */
+    private $colunas;
 
     public function __construct(string $caminhoArquivo)
     {
         if(!file_exists($caminhoArquivo)) {
             throw new \InvalidArgumentException("O caminho do arquivo é inválido!");
         }
+        $linhas = explode("\n", file_get_contents($caminhoArquivo));
+        $this->colunas = explode("\t", $linhas[0]);
+        array_shift($linhas);
+        $this->data = array_map(
+            function ($linha) {
+                return array_combine($this->colunas, explode("\t", $linha));
+            },
+            $linhas
+        );
 
     }
 
     public function getColunas(): array
     {
-        return [];
+        return $this->colunas ?? [];
     }
 
     public function getValorLinhaColuna(int $numeroLinha, string $nomeColuna): string
     {
-        return "";
+        if (!in_array($nomeColuna, $this->colunas)) {
+            throw new \InvalidArgumentException("Nome de coluna inválido!");
+        }
+        if (!isset($this->data[$numeroLinha])) {
+            throw new \InvalidArgumentException("Número de linha inexistente!");
+        }
+        return $this->data[$numeroLinha][$nomeColuna];
     }
 }
